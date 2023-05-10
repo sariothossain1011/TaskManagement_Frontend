d9638108bfaa8b29560d72bd989936cdf7ed20c7
Sariot Hossain <106438276+sariothossain1011@users.noreply.github.com>
2023-05-11 00:49:18 +0600

    not found file added

--- src/Components/Notfound/Notfound.js
+++ src/Components/Notfound/Notfound.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Notfound = () => {
-    return (
-        <div>
-            
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate("/");
+    }, 5000); // redirect to home page after 5 seconds
+
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+
+  const handleBackClick = () => {
+    navigate.goBack();
+  };
+
+  return (
+    <div className="image-div">
+      <div className="text-section">
+        <div className="text-div">
+          <h1>Page Not Found</h1>
+          <p>The page you are looking for does not exist.</p>
+          <button className="btn btn-primary btn-small" onClick={handleBackClick}>Go Back</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Notfound;
